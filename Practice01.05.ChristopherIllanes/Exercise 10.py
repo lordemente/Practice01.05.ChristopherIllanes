26186a3f36498a04b5f1cf58abcfc22e1e948cc6
Christopher <144793737+lordemente@users.noreply.github.com>
2023-09-30 16:30:17 +0200

    exercise 10

---
+++ Practice01.05.ChristopherIllanes/Exercise 10.py
@@ -0,0 +1,21 @@
+
+número_de_payasos = int(input("Número de payasos vendidos"))
+número_de_muñecas = int(input("Número de muñecas vendidas"))
+peso_Payaso = 112 / 1000
+peso_muñeca = 75 / 1000
+
+Juguetes_vendidos = (número_de_payasos + número_de_muñecas)
+print("juguetes vendidos", Juguetes_vendidos)
+
+
+Peso_total = (número_de_payasos * peso_Payaso) + (número_de_muñecas * peso_muñeca)
+
+print(número_de_payasos, "payasos vendidos")
+print(número_de_muñecas, "muñecas vendidads")
+
+print("peso de la caja", Peso_total, "kg")
+
+
+
+
+
