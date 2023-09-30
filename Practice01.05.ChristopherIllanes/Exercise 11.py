47f16069b1fab2ad8fc0021872a45740720a8084
Christopher <144793737+lordemente@users.noreply.github.com>
2023-09-30 15:50:58 +0200

    exercise 11.py

--- Practice01.05.ChristopherIllanes/Exercise 11.py
+++ Practice01.05.ChristopherIllanes/Exercise 11.py
@@ -1,9 +1,14 @@
-Interes_anual = 40/100 
+Interes_anual = 4/100 
 
-Cantidad_1 = float(input("Cantidad de deposito de dinero de ahorros"))
+Cantidad = float(input("Cantidad del deposito de dinero de ahorros"))
 
-Primer_año = Cantidad_1 * Interes_anual
-segundo_año = Primer_año / Interes_anual
+
+Primer_año = Cantidad * (1 + Interes_anual)
+segundo_año = Primer_año * (1 + Interes_anual)
+
+tercer_año = segundo_año * (1 + Interes_anual)
 
 print(Primer_año)
 print(segundo_año)
+print(tercer_año)
+
